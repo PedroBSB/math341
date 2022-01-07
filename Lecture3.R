97089bd105f2307271fe9dabda75715d8bb64a5f
Pedro Albuquerque <pedro.melo.albuquerque@gmail.com>
2022-01-06 20:51:54 -0800

    Problem 1

---
+++ Lecture3.R
@@ -0,0 +1,28 @@
+##########   Same birthday problem   ##########   
+
+#First step: Define the sample size of the simulation
+sample_size = 1000
+
+#Generate 25 random numbers between 1 and 365
+list_days = seq(1,365)
+numbers = sample(list_days, size = 25, replace = TRUE)
+
+#Count equal numbers
+equal = !(length(unique(numbers)) == length(numbers))
+
+#Create a loop
+numerator = 0
+for(sample in 1:sample_size){
+  #Generate 25 random numbers between 1 and 365
+  list_days = seq(1,365)
+  numbers = sample(list_days, size = 25, replace = TRUE)
+  
+  #Count equal numbers
+  equal = !(length(unique(numbers)) == length(numbers))
+  
+  #Compute numerator
+  numerator = numerator + equal
+}
+
+#Final probability
+numerator/sample_size
