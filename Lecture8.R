47076b88a254b70d6dcccb2e3a4e269673dd3dc1
Pedro Albuquerque <pedro.melo.albuquerque@gmail.com>
2022-03-05 15:15:02 -0800

    ANOVA Exercises

--- Lecture8.R
+++ Lecture8.R
@@ -21,3 +21,32 @@ ggplot(df_panel, aes(x = value, fill = key)) +
 options(digits=10)
 res = aov(value~key,data=df_panel)
 summary(res)
+
+## Exercise:
+#A large body of evidence shows that soy has health benefits for most people. Some of these benefits come largely from isoflavones, plant compounds that have estrogen-like properties. 
+data = data.frame(group=c(1,1,1,1,1,2,2,2,2,2,3,3,3,3,3),
+                  isoflavone=c(3,17,12,10,4,19,10,9,7,5,25,15,12,9,8))
+res = aov(isoflavone~group,data=data)
+summary(res)
+
+
+## Exercise :
+#Because many HMOs either do not cover mental health costs or provide only minimal coverage, ministers and priests often need to provide counseling to persons suffering from mental illness.
+library(dplyr)
+cleric = data.frame(group=c(rep("Methodist",10),rep("Catholic",10),rep("Pentecostal",10)),
+                  value=c(62,60,60,25,24,23,20,13,12,6,
+                          62,62,24,24,22,20,19,10,8,8,
+                          37,31,15,15,14,14,14,5,3,2))
+#Get statistics by group
+cleric %>% group_by(group) %>% summarise(mean(value))
+cleric %>% group_by(group) %>% summarise(sd(value))
+cleric %>% group_by(group) %>% summarise(n())
+cleric %>% group_by(group) %>% summarise(median(value))
+
+#Run the ANOVA model
+res = aov(value~group,data=cleric)
+
+#Check the results
+summary(res)
+
+
